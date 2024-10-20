6728d85d58615dc3dbdde70d475262356a814d34
Cody Sorgenfrey <codysorgenfrey+github@gmail.com>
2024-10-19 20:04:36 -0700

    Remove unneeded api endpoints

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,3 @@
-import metadata from './metadata.js';
 import proxy from './proxy.js';
-import suggest from './suggest.js';
-import weather from './weather.js';
 
-export { metadata, proxy, suggest, weather };
+export { proxy };
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
-import { suggest, proxy, metadata, weather } from './api/index.js';
+import { proxy } from './api/index.js';
 
 dotenv.config(); // Load .env file for keys
 
@@ -8,10 +8,7 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 // Handle GET requests for api endpoints
-app.get('/api/weather', weather);
-app.get('/api/suggest', suggest);
-app.get('/api/proxy', proxy);
-app.get('/api/metadata', metadata);
+app.get('/', proxy);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
